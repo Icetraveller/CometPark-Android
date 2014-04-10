9e996c1ca46b88f6e26c8fdf60d14f125a2e2a59
Elonix Thonarel <spider0quoll@gmail.com>
2014-04-09 21:09:32 -0500

    clean up

--- gen/com/icetraveller/android/apps/cometpark/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.icetraveller.android.apps.cometpark;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
