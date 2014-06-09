8a92e967a5cc99a412952e13c9ecdce52e0c065f
Elonix Thonarel <spider0quoll@gmail.com>
2014-06-09 10:42:55 -0500

    improve donate

--- src/com/icetraveller/android/apps/cometpark/ui/AboutActivity.java
+++ src/com/icetraveller/android/apps/cometpark/ui/AboutActivity.java
@@ -1,27 +1,107 @@
 package com.icetraveller.android.apps.cometpark.ui;
 
+import com.android.inapppurchase.util.IabHelper;
+import com.android.inapppurchase.util.IabResult;
+import com.android.inapppurchase.util.Inventory;
+import com.android.inapppurchase.util.Purchase;
 import com.icetraveller.android.apps.cometpark.R;
+import com.icetraveller.android.apps.cometpark.utils.BillUtil;
+import com.icetraveller.android.apps.cometpark.utils.LogUtils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class AboutActivity extends Activity {
+
+	private static final String TAG = LogUtils.makeLogTag(AboutActivity.class);
+
+	private Button donateButton;
+	private IabHelper billHelper;
+	private Context context = this;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about_activity);
+		findUI();
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    switch (item.getItemId()) {
-	    // Respond to the action bar's Up/Home button
-	    case android.R.id.home:
-	        finish();
-	        return true;
-	    }
-	    return super.onOptionsItemSelected(item);
+		switch (item.getItemId()) {
+		// Respond to the action bar's Up/Home button
+		case android.R.id.home:
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void findUI() {
+		donateButton = (Button) findViewById(R.id.donate);
+		donateButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startIAB();
+			}
+		});
 	}
 	
+	private void startIAB() {
+		Log.i(TAG, "create IAB Helper");
+		billHelper = new IabHelper(this, BillUtil.base64EncodedPublicKey);
+		// TODO disable this
+		billHelper.enableDebugLogging(true, TAG);
+
+		Log.d(TAG, "Starting setup");
+		billHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+			@Override
+			public void onIabSetupFinished(IabResult result) {
+				Log.d(TAG, "Setup finished");
+				if (!result.isSuccess()) {
+					BillUtil.complain(context, TAG,
+							"Problem setting up in-app billing: " + result);
+					return;
+				}
+
+				if (billHelper == null) {
+					return;
+				}
+
+				Log.d(TAG, "Setup successfully, querying inventory");
+				billHelper.queryInventoryAsync(mGotInventoryListener);
+			}
+		});
+	}
+
+	
+
+	// query items that we own
+	IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+		@Override
+		public void onQueryInventoryFinished(IabResult result, Inventory inv) {
+			Log.d(TAG, "Query inventory finished.");
+
+			if (billHelper == null) {
+				return;
+			}
+
+			if (!result.isSuccess()) {
+				BillUtil.complain(context, TAG, "Failed to query inventory: "
+						+ result);
+			}
+
+			Log.d(TAG, "Query Inventory was successful.");
+			Purchase oneDollarPurchase = inv
+					.getPurchase(BillUtil.SKU_DONATION_ONE);
+
+		}
+	};
+
 }
--- src/com/icetraveller/android/apps/cometpark/utils/BillUtil.java
+++ src/com/icetraveller/android/apps/cometpark/utils/BillUtil.java
@@ -1,7 +1,79 @@
 package com.icetraveller.android.apps.cometpark.utils;
 
+
+import com.icetraveller.android.apps.cometpark.R;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
 public class BillUtil {
 	public static final String SKU_DONATION_ONE = "one_dollar";
 	public static final String SKU_DONATION_FIVE = "five_dollar";
 
+	public static String DONATE_FRAGMENT_TAG = "dialog_dondate";
+
+	public static final int RC_REQUEST = 10001;
+
+	public static String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp8wXzhC7OAMMPGkbbRbEMs3Z9a41pAvCsk7SK+ebYmtQnt8gw60ipljrAcWrbEF0tdTBKoknMOQAebwHGNRi2jwvexPZv4F8oYHKcbfbHO7ysQwwuMJnpF5+soEPGyo5Xpptg1lddpSlqiJDi5+Ke0D+hEzoCtaS0w2j8fNRacnC4MO7IgkggTVSAHVuA8eu8yqaKIlNL+5o79noqhLnleX1p3gHyRY+FBxdjPXQZhpEaEpVly4UJcSNRB96fkUIUIH3j1OArIxyTF8GS9TCAdW1xggANbKBrgBAGXMsbTbkYCbHIW8oqqYVCjhv2Z3GyoeS3/RsngYBo/Wro/d5GQIDAQAB";
+
+	public static void complain(Context context, String TAG, String message) {
+		Log.e(TAG, "**** TrivialDrive Error: " + message);
+		alert(context, TAG, "Error: " + message);
+	}
+
+	private static void alert(Context context, String TAG, String message) {
+		AlertDialog.Builder bld = new AlertDialog.Builder(context);
+		bld.setMessage(message);
+		bld.setNeutralButton("OK", null);
+		Log.d(TAG, "Showing alert dialog: " + message);
+		bld.create().show();
+	}
+
+	public static void showAbout(FragmentActivity activity) {
+		FragmentManager fm = activity.getSupportFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		Fragment prev = fm.findFragmentByTag(DONATE_FRAGMENT_TAG);
+		if (prev != null) {
+			ft.remove(prev);
+		}
+		ft.addToBackStack(null);
+
+		new DonationDialog().show(ft, DONATE_FRAGMENT_TAG);
+	}
+
+	public static class DonationDialog extends DialogFragment {
+		public DonationDialog() {
+
+		}
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			 LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(
+	                    Context.LAYOUT_INFLATER_SERVICE);
+			TextView donationView = (TextView) layoutInflater.inflate(R.layout.donate_dialog, null);
+			return new AlertDialog.Builder(getActivity())
+            .setTitle(R.string.donate)
+            .setView(donationView)
+            .setPositiveButton(R.string.ok,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            dialog.dismiss();
+                        }
+                    }
+            )
+            .create();
+		}
+
+	}
 }
